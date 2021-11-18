e94ef455df5d6285a31d13875157342646c4bab7
Jim Bumgardner <jbum@jbum.com>
2021-11-18 09:58:15 -0800

    Added cosmetic time rounding to status updates.

--- src/logUpdateTime.js
+++ src/logUpdateTime.js
@@ -9,7 +9,25 @@ function writeFile(file, filecontent) {
 
 let filename = process.argv[2];
 if (filename.startsWith("./status/")) {
-  let jsonData = {'PUBLISH_DATE':(new Date()).toISOString()};
+  let uDate = new Date();
+
+  // add 15 minutes to account for publishing delay
+  let addMS = 15 * 60 * 1000;
+  uDate = new Date(uDate.getTime() + addMS);
+
+  // set seconds/milliseconds to zero for prettiness
+  let trimMS = uDate.getMilliseconds() + uDate.getSeconds()*1000;
+  uDate = new Date(uDate.getTime() - trimMS);
+
+  // round to next half hour for prettiness
+  let minutes = uDate.getMinutes();
+  if (minutes % 30 != 0) {
+    let extraMinutes = 30 - (minutes % 30);
+    let addMS = extraMinutes * 60 * 1000;
+    uDate = new Date(uDate.getTime() + addMS);
+  }
+
+  let jsonData = {'PUBLISH_DATE':uDate.toISOString()};
   // write a new date file to make sure script runs even if there is no svg change  
   writeFile(filename, JSON.stringify(jsonData));
 }
